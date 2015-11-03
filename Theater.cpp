7a2144ace5fe3a90d37b8d5db4220f6e93a867e2
NSuri1 <nalinsuri@gmail.com>
2015-11-03 10:11:00 -0500

    Completed my Function

--- Theater.cpp
+++ Theater.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 const int rows= 15;
@@ -27,8 +28,12 @@ int main(){
 }
 
 
-void enterSeatPrices(double (&array)[rows])
+void enterSeatPrices(double (&array)[rows]) //Function done by Nalin Suri
 {
-	
+	for (int count=0;count<rows; count++)
+	{
+		cout<< "Please enter ticket price for Row"<< setw(2) << rows+1;
+		cin>> array[rows];
+	}
 	
 }
