c9e9a9289b99c30f282b2dc836d0a1e5f49951e8
Nick Perez <nperez0111@gmail.com>
2015-11-03 09:51:51 -0500

    nalin

--- Theater.cpp
+++ Theater.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 void enterSeatPrices(int &[]);
 //first function to be called, asks users for price of each row
+void menu();
+//menu caller
+
 int main(){
 	
 	system("PAUSE");
